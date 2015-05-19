1951760cae71831a015fafe484c05b4d173c09da
Lee A. Lewis <llewis@fullsail.com>
2015-05-19 13:15:47 -0400

    LabSeven code

---
+++ LabSeven/js/lewis_lee_lab7.js
@@ -0,0 +1,55 @@
+/**
+ * Created by llewis on 5/19/15.
+ */
+
+//alert("Yes!");
+
+// global variables
+var array1 = [ 10, 6, 7, 3, 12, 5 ];
+var array2 = [ 25, 100, 4, 22, 16, 105, 2, 7 ];
+var array3 = [ 2, 3, 4, 1, 10, 27, 32, 51, 6, 100, 8, 42 ]
+
+//function
+function bubbleSort(argArray) {
+
+    // local variables
+    var newArray;
+    var num1, num2;
+    var holder;
+
+    for (var y = 0; y < argArray.length; y++) {
+        for (var i = 0; i < argArray.length; i++) {
+
+            num1 = argArray[i];
+            num2 = argArray[i + 1];
+
+            if (num1 > num2) {
+
+                holder = num2;
+                argArray[i] = num2;
+                argArray[i + 1] = num1;
+
+            }
+
+            //console.log(argArray);
+
+        }
+    }
+
+    return argArray;
+}
+
+// main code
+for (var x = 0; x < 3; x++) {
+
+    if (x === 0) {
+        returnedArray = bubbleSort(array1);
+    } else if (x === 1) {
+        returnedArray = bubbleSort(array2);
+    } else {
+        returnedArray = bubbleSort(array3);
+    }
+
+    console.log("The sorted array is " + returnedArray);
+
+}
