4f4612e4000998a9eaeb6d7f6f01566d31239795
Lee A. Lewis <llewis@fullsail.com>
2015-05-19 10:17:43 -0400

    first commit for evenArray problem

--- DaySeven/js/script.js
+++ DaySeven/js/script.js
@@ -3,32 +3,89 @@
 //  SDI 1505
 //  More arrays, loops, and functions
 
+
+// global variables
+var numberArray = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ];
+var returnedArray = [];
+
+
+
+// function
+function evenArray(argArray) {
+
+    // local variables
+    var newArray = [];
+    var modNum;
+
+    console.log(argArray);
+
+}
+
+
+// main code
+returnedArray = evenArray(numberArray);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //alert("JavaScript works!");
 
 // Lab six review
 
 // function
-function authorFunction() {
-
-    // local arrays
-    var authors = [ "Charles Dickens", "William Shakespeare", "Mark Twain", "Hunter S. Thompson"];
-    var titles  = [ "Oliver Twist", "Hamlet", "Tom Sawyer", "Fear and Loathing in Las Vegas"];
-
-    // for loop to go through the array items
-    //   output each element
-    for (var i = 0; i < authors.length; i++) {
-
-        console.log(authors[i] + " wrote " + titles[i] + ".");
-
-    }
-
-    // use an array method to alter the arrays
-
-
-    // do a final output of the change
-
-
-}
-
-// main code
-authorFunction();
+//function authorFunction() {
+//
+//    // local arrays
+//    var authors = [ "Charles Dickens", "William Shakespeare", "Mark Twain", "Hunter S. Thompson"];
+//    var titles  = [ "Oliver Twist", "Hamlet", "Tom Sawyer", "Fear and Loathing in Las Vegas"];
+//
+//    // for loop to go through the array items
+//    //   output each element
+//    for (var i = 0; i < authors.length; i++) {
+//
+//        console.log(authors[i] + " wrote " + titles[i] + ".");
+//
+//    }
+//
+//    // use an array method to alter the arrays
+//    authors.push("Dorothy Allison");
+//    titles.push("Bastard Out of Carolina");
+//
+//    // do a final output of the change
+//    console.log("I added " + authors[authors.length - 1] + " who wrote " + titles[authors.length - 1] + ".");
+//
+//}
+//
+//// main code
+//authorFunction();
