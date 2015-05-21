17732fe90474bd9177085639d6abc036dfa1f3c2
Lee A. Lewis <llewis@fullsail.com>
2015-05-21 09:14:28 -0400

    Commit of files

---
+++ DayEight/js/script.js
@@ -0,0 +1,24 @@
+/*
+  Lee A. Lewis
+  SDI 1505
+  Lab 7 Review
+  Day Eight:  Playing with strings like cats
+ */
+
+
+//  Lab Seven Review code
+
+//alert("JavaScript works!");
+
+// Global variables
+var array1 = [ 10, 6, 7, 3, 12, 5, 1 ];
+var array2 = [ 25, 100, 4, 22, 16, 105, 2, 7 ];
+var array3 = [ 2, 3, 4, 1, 10, 27, 32, 51, 6, 100, 8, 42 ];
+
+function bubbleSort(argArray) {
+
+  console.log(argArray);
+
+}
+
+bubbleSort(array1);
--- LabSeven/js/lewis_lee_lab7.js
+++ LabSeven/js/lewis_lee_lab7.js
@@ -5,7 +5,7 @@
 //alert("Yes!");
 
 // global variables
-var array1 = [ 10, 6, 7, 3, 12, 5 ];
+var array1 = [ 10, 6, 7, 3, 12, 5, 1 ];
 var array2 = [ 25, 100, 4, 22, 16, 105, 2, 7 ];
 var array3 = [ 2, 3, 4, 1, 10, 27, 32, 51, 6, 100, 8, 42 ]
 
@@ -31,7 +31,7 @@ function bubbleSort(argArray) {
 
             }
 
-            //console.log(argArray);
+            console.log(argArray);
 
         }
     }
