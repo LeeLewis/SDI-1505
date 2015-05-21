150f4df32b2eef94d9ab8d923e6d7bd597824d0c
Lee A. Lewis <llewis@fullsail.com>
2015-05-21 16:03:46 -0400

    Commit again

--- Day8New/Day-8-lecture.js
+++ Day8New/Day-8-lecture.js
@@ -2,6 +2,90 @@
 //  Day 8 Lecture
 //  Like cats, we're going to play with strings
 
+
+// global variable
+var ourString;
+var returnedString
+
+// functions
+function initialCaps(argString) {
+
+    // local variables
+    var newString;
+    var strArray;
+    var currentWord;
+    var newWord = "";
+
+    // uppercase everything
+    newString = argString.toUpperCase();
+
+    strArray = newString.split(" ");
+
+    for (var each in strArray) {
+
+        currentWord = strArray[each];
+
+        for (var char in strArray[each]) {
+
+            if (char != 0) {
+
+                currentChar = currentWord[char].toLowerCase();
+                newWord = newWord + currentChar;
+
+            } else {
+
+                newWord = newWord + currentWord[0];
+
+            }
+
+        }
+
+        newWord = newWord + " ";
+
+    }
+
+    return newWord;
+
+}
+
+ourString = prompt("Enter a string to initial cap: ", "i am batman");
+
+returnedString = initialCaps(ourString);
+
+console.log(returnedString);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Using .indexOf()
 //  Find if the substring "Adam" appears in the string and where
 var myIndexOf = "Madam, I'm Adam.";
@@ -26,4 +110,72 @@ var myEmail = "llewis@fullsail.com";
 //  Split a string into an array and output each item individually
 var mySplit = "Now is the time for all good men to come to the aid of the party.";
 
+
+
+// functions to do string string manipulation
+function wheresAdam(whereIsIt) {
+
+    var where = whereIsIt.lastIndexOf("dam");
+    var where2 = whereIsIt.indexOf("@");
+
+    return [where, where2];
+
+}
+
+function subStrFunction(hitchArg) {
+
+    var theT = hitchArg.indexOf("t");
+    var theComma = hitchArg.indexOf(",", theT);
+
+    var theUniverse = hitchArg.substring(theT, theComma);
+    return theUniverse;
+
+}
+
+function substrFunction(hitchArg) {
+
+    var everything = hitchArg.substr(24, 5);
+
+    return everything;
+
+}
+
+function charAtFunction(emailArg) {
+
+    var atSymbol = emailArg.charAt(6);
+
+    return atSymbol;
+
+}
+
+function splitString(stringToSplit) {
+
+    var splitArray = stringToSplit.split(" ", 4);
+
+    return splitArray;
+
+}
+
+function changeCase(caseString) {
+
+    var newString = caseString.toLowerCase();
+
+    return newString;
+
+}
+
+// main code
+//console.log("The last and first indices are " + wheresAdam(myIndexOf));
+//console.log("We've returned \"" + substrFunction(mySubString) + "\"");
+//console.log("The character at index 6 is " + charAtFunction(myEmail));
+//var returnedArray = splitString(mySplit);
+//console.log(returnedArray);
+//
+//for (var each in returnedArray) {
+//
+//    console.log(returnedArray[each]);
+//
+//}
+
+console.log(changeCase(mySplit));
 
--- DayEight/js/script.js
+++ DayEight/js/script.js
@@ -14,11 +14,50 @@
 var array1 = [ 10, 6, 7, 3, 12, 5, 1 ];
 var array2 = [ 25, 100, 4, 22, 16, 105, 2, 7 ];
 var array3 = [ 2, 3, 4, 1, 10, 27, 32, 51, 6, 100, 8, 42 ];
+var numArrays = [array1, array2, array3];
+var returnedArray;
 
 function bubbleSort(argArray) {
 
-  console.log(argArray);
+  // local variables
+  var num1,
+      num2,
+      holder;
+
+  // loop through the array
+  for (var newElement in argArray) {
+
+      for (var element in argArray) {
+
+          element = parseInt(element);
+
+          // assign array elements to variables
+          num1 = argArray[element];
+          num2 = argArray[element + 1];
+
+          // compare to see which is greater
+          if (num1 > num2) {
+
+            holder = num1;
+            argArray[element] = num2;
+            argArray[element + 1] = num1;
+
+          }
+
+      }
+  }
+
+  return argArray;
 
 }
 
-bubbleSort(array1);
+//  Main Code
+for (var each in numArrays) {
+
+  returnedArray = bubbleSort(numArrays[each]);
+  console.log("Array number " + each + " sorted is " + returnedArray);
+
+}
+
+
+
